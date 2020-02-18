5d26c9c51abe0b38002c2e8610b3fa0de249c9e5
Nathan Lövsund <nathan@lovsund.eu>
2020-02-18 17:28:41 +0100

    Add iterator sample with Fibonacci

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fun-with-python
+
 Cool and useful pythonic code
+
+## Iterators
+
+Example with Fibonacci sequence iterator on how to create them in a pythonic
+way.
---
+++ iterator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+class Fibonacci:
+    def __init__(self, max):
+        self.max = max
+
+    def __iter__(self):
+        self.first = 0
+        self.second = 1
+        return self
+
+    def __next__(self):
+        fib = self.first
+        if fib > self.max:
+            raise StopIteration
+        self.first, self.second = self.second, self.first + self.second
+        return fib
+
+def main():
+    print("\nLimited Fibonacci sequence:")
+    for num in Fibonacci(500):
+        print(num)
+
+if __name__ == "__main__":
+    main()
