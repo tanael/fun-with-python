1f94e082b3239d627191834fb596402435078baa
Nathan Lövsund <nathan@lovsund.eu>
2020-03-02 21:28:43 +0100

    Update coroutine sample

--- coroutines/coroutine.py
+++ coroutines/coroutine.py
@@ -5,9 +5,11 @@ def print_tag(tag):
     idx = 0
     try:
         while True:
+            print("Reaching the yield statement")
             line = (yield "{} I yield".format(idx))
+            print("Passed the yield statement")
             if tag in line:
-                print(line)
+                print("Matched the tag: {}".format(line))
             idx += 1
     except GeneratorExit:
         print("Closing the coroutine.")
@@ -15,19 +17,19 @@ def print_tag(tag):
 def main():
     coro = print_tag("hyper")
     caught = coro.__next__() # to start execution of coroutine, till the first yield
-    print(caught)
+    print(">> 0 Caught this: {}".format(caught))
     caught = coro.send("space")
-    print(caught)
+    print(">> 1 Caught this: {}".format(caught))
     caught = coro.send("hyperspace")
-    print(caught)
+    print(">> 2 Caught this: {}".format(caught))
     caught = coro.send("superspace")
-    print(caught)
+    print(">> 3 Caught this: {}".format(caught))
     caught = coro.send("I like hyperspace")
-    print(caught)
+    print(">> 4 Caught this: {}".format(caught))
     coro.close()
     try:
-        caught = coro.send("I like hyperspace")
-        print(caught)
+        caught = coro.send("I like hyperspace a lot")
+        print(">> 5 Caught this: {}".format(caught))
     except StopIteration:
         print("Coroutine was closed")
 
