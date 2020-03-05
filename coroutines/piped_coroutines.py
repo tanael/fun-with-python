239a61545a197a432506f4d58622df9978f04939
Nathan Lövsund <nathan@lovsund.eu>
2020-03-05 11:47:25 +0100

    Start piped coroutine sample
    Work in progress.

--- coroutines/piped_coroutines.py
+++ coroutines/piped_coroutines.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
-def main():
+def producer(next_coroutine=None):
     pass
+
+def coro1(next_coroutine=None):
+    pass
+
+def coro2(next_coroutine=None):
+    pass
+
+def sink():
+    pass
+
+def main():
+    mysink = sink()
+    mysink.send(None)
+    c2 = coro2(next_coroutine = mysink)
+    c2.__next__()
+    c1 = coro1(next_coroutine = coro2)
+    c1.__next__()
+
+    producer(next_coroutine = coro1)
 
 if __name__ == "__main__":
     main()
