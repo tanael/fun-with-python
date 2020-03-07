292410c4b26d635390743a2abac1e32c14189a61
Nathan Lövsund <nathan@lovsund.eu>
2020-03-07 12:56:37 +0100

    Update piped coroutine sample
    Sample finished

--- coroutines/piped_coroutines.py
+++ coroutines/piped_coroutines.py
@@ -1,26 +1,55 @@
 #!/usr/bin/env python3
 
-def producer(next_coroutine=None):
-    pass
+def producer(phrase, next_coroutine=None):
+    print("Producing tokens")
+    tokens = phrase.split(" ")
+    for token in tokens:
+        next_coroutine.send(token)
+    next_coroutine.close()
+    print("Token production terminated")
 
-def coro1(next_coroutine=None):
-    pass
+def filter_pattern(pattern="er", next_coroutine=None):
+    print("Filtering using pattern")
+    try:
+        while True:
+            token = yield
+            if pattern in token:
+                next_coroutine.send(token)
+    except GeneratorExit:
+        print("Done with 1st coroutine")
+        next_coroutine.close()
 
-def coro2(next_coroutine=None):
-    pass
+def decorate_token(next_coroutine=None):
+    print("Decorating token")
+    try:
+        while True:
+            token = yield
+            token = ">> {} <<".format(token)
+            next_coroutine.send(token)
+    except GeneratorExit:
+        print("Done with 2nd coroutine")
+        next_coroutine.close()
 
 def sink():
-    pass
+    print("Welcome to the sink")
+    try:
+        while True:
+            token = yield
+            print(token)
+    except GeneratorExit:
+        print("Sink terminated")
 
 def main():
     mysink = sink()
     mysink.send(None)
-    c2 = coro2(next_coroutine = mysink)
-    c2.__next__()
-    c1 = coro1(next_coroutine = coro2)
-    c1.__next__()
+    decotok = decorate_token(next_coroutine = mysink)
+    decotok.__next__()
+    filpat = filter_pattern(next_coroutine = decotok)
+    filpat.__next__()
 
-    producer(next_coroutine = coro1)
+    phrase = "The Destroyer of Worlds has come, fellow citizens. Show him no mercy!"
+    producer(phrase, next_coroutine = filpat)
+    print("Demonstration over")
 
 if __name__ == "__main__":
     main()
